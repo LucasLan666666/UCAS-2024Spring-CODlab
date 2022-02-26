c762b068af47007d877b8a44c367d46bf61d90c5
Yisong Chang <changyisong@ict.ac.cn>
2022-02-26 21:52:03 +0800

    flow: synth: run check_timing to find potential combinational loops

--- fpga/design/ucas-cod/fpga/scripts/flow/role/synth.tcl
+++ fpga/design/ucas-cod/fpga/scripts/flow/role/synth.tcl
@@ -2,6 +2,9 @@
 synth_design -top custom_role -part ${device} -mode out_of_context \
     -directive default -flatten_hierarchy rebuilt -max_uram 0
 
+# checking potential combinational loops
+check_timing -verbose
+
 # setup output logs and reports
 report_utilization -hierarchical -file ${synth_rpt_dir}/synth_util_hier.rpt
 report_utilization -file ${synth_rpt_dir}/synth_util.rpt
