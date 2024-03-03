aaed5b0076eba5dff5ee8bbbf7456cf3003ebb4a
Yuzhou Lan <lanyuzhou22@mails.ucas.ac.cn>
2024-03-04 04:12:32 +0800

    reg_file & alu 1

--- fpga/design/ucas-cod/hardware/sources/alu/alu.v
+++ fpga/design/ucas-cod/hardware/sources/alu/alu.v
@@ -1,16 +1,61 @@
 `timescale 10 ns / 1 ns
 
 `define DATA_WIDTH 32
+`define AND        3'b000
+`define OR         3'b001
+`define ADD        3'b010
+`define SUB        3'b110
+`define SLT        3'b111
 
-module alu(
-	input  [`DATA_WIDTH - 1:0]  A,
-	input  [`DATA_WIDTH - 1:0]  B,
-	input  [              2:0]  ALUop,
-	output                      Overflow,
-	output                      CarryOut,
-	output                      Zero,
-	output [`DATA_WIDTH - 1:0]  Result
+module alu (
+    input  [`DATA_WIDTH - 1:0]  A,
+    input  [`DATA_WIDTH - 1:0]  B,
+    input  [              2:0]  ALUop,
+    output                      Overflow,
+    output                      CarryOut,
+    output                      Zero,
+    output [`DATA_WIDTH - 1:0]  Result
 );
-	// TODO: Please add your logic design here
 
+    wire   [`DATA_WIDTH - 1:0]  A2;
+    wire   [`DATA_WIDTH - 1:0]  B2;
+    wire                        cout;
+    wire   [`DATA_WIDTH - 1:0]  sum;
+
+    adder alu_adder (
+        .A(         A2),
+        .B(         B2),
+        .cin(        0),
+        .cout(    cout),
+        .sum(      sum)
+    );
+
+    // 一个加法器实现 ADD，SUB，SLT
+    assign       A2 = A;
+    assign       B2 = (ALUop == `SUB || ALUop == `SLT) ? ~B + 1: B;
+
+    assign CarryOut = (ALUop == `ADD) ? cout
+                    : (ALUop == `SUB) ? (~A[`DATA_WIDTH - 1] && B[`DATA_WIDTH - 1]) || (~A[`DATA_WIDTH - 1] && ~B[`DATA_WIDTH - 1] && sum[`DATA_WIDTH - 1]) || (A[`DATA_WIDTH - 1] && B[`DATA_WIDTH - 1] && ~sum[`DATA_WIDTH - 1])
+                    : `DATA_WIDTH'bz;
+
+    assign Overflow = (ALUop == `ADD || ALUop == `SUB) ? (A2[`DATA_WIDTH - 1] && B2[`DATA_WIDTH - 1] && ~sum[`DATA_WIDTH - 1]) || (~A2[`DATA_WIDTH - 1] && ~B2[`DATA_WIDTH - 1] && sum[`DATA_WIDTH - 1])
+                    : `DATA_WIDTH'bz;
+
+    assign     Zero = (Result == `DATA_WIDTH'b0);
+
+    assign   Result = (ALUop == `AND) ? A & B
+                    : (ALUop == `OR ) ? A | B
+                    : (ALUop == `ADD || ALUop == `SUB) ? sum
+                    : (ALUop == `SLT) ? sum[`DATA_WIDTH - 1] ^^ Overflow
+                    : `DATA_WIDTH'bz;
+endmodule
+
+module adder (
+    input  [`DATA_WIDTH - 1:0]  A,
+    input  [`DATA_WIDTH - 1:0]  B,
+    input                       cin,
+    output                      cout,
+    output                      sum
+);
+    assign {cout, sum} = A + B + cin;
 endmodule
--- fpga/design/ucas-cod/hardware/sources/reg_file/reg_file.v
+++ fpga/design/ucas-cod/hardware/sources/reg_file/reg_file.v
@@ -2,6 +2,7 @@
 
 `define DATA_WIDTH 32
 `define ADDR_WIDTH 5
+`define   ADDR_NUM 32 // 寄存器数量是 32
 
 module reg_file(
 	input                       clk,
@@ -14,6 +15,17 @@ module reg_file(
 	output [`DATA_WIDTH - 1:0]  rdata2
 );
 
-	// TODO: Please add your logic design here
-	
+	// 寄存器堆
+	reg [`DATA_WIDTH - 1:0] mem [`ADDR_NUM - 1:0];
+
+	// clk 上升沿，根据 wen 判断是否接受输入
+	always @(posedge clk) begin
+		if (wen == 1) begin
+			mem[waddr][`DATA_WIDTH] <= wdata;
+		end
+	end
+
+	// 读出指定地址数据
+	assign rdata1 = (raddr1 == 0) ? 0 : mem[raddr1][`DATA_WIDTH];
+	assign rdata2 = (raddr2 == 0) ? 0 : mem[raddr2][`DATA_WIDTH];
 endmodule
