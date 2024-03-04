0d4f544bb408ffdc244d85598b972c9a6b41bfcc
Yuzhou Lan <lanyuzhou22@mails.ucas.ac.cn>
2024-03-04 21:50:37 +0800

    alu test

--- fpga/design/ucas-cod/hardware/sources/alu/alu.v
+++ fpga/design/ucas-cod/hardware/sources/alu/alu.v
@@ -18,29 +18,32 @@ module alu (
     localparam SUB = 3'b110;
     localparam SLT = 3'b111;
 
-    wire   [`DATA_WIDTH - 1:0]  A2;
-    wire   [`DATA_WIDTH - 1:0]  B2;
     wire                        cout;
     wire   [`DATA_WIDTH - 1:0]  sum;
+    wire                        B_Tmin;
 
     adder_32 alu_adder (
-        .A(         A2),
-        .B(         B2),
+        .A(          A),
+        .B(          B),
         .cin( b_invert),
         .cout(    cout),
         .sum(      sum)
     );
 
-    // 一个加法器实现 ADD，SUB，SLT
-    assign       A2 = A;
-    assign       B2 = (ALUop == SUB || ALUop == SLT) ? ~B : B;
-    assign b_invert = (ALUop == SUB || ALUop == SLT) ? 1 : 0;
+    assign b_invert = (ALUop == SUB || ALUop == SLT) ?  1 : 0;
+    // B 是否为补码最小数
+    assign   B_Tmin = (A[`DATA_WIDTH - 1] ^ sum[`DATA_WIDTH - 1]) && (A[`DATA_WIDTH - 2:0] ^ sum[`DATA_WIDTH - 2:0] == 0);
 
     assign CarryOut = (ALUop == ADD) ? cout
-                    : (ALUop == SUB) ? (~A[`DATA_WIDTH - 1] && B[`DATA_WIDTH - 1]) || (~A[`DATA_WIDTH - 1] && ~B[`DATA_WIDTH - 1] && sum[`DATA_WIDTH - 1]) || (A[`DATA_WIDTH - 1] && B[`DATA_WIDTH - 1] && ~sum[`DATA_WIDTH - 1])
+                    : (ALUop == SUB) ? (~A[`DATA_WIDTH - 1] &&  B[`DATA_WIDTH - 1])
+                                    || (~A[`DATA_WIDTH - 1] && ~B[`DATA_WIDTH - 1] &&  sum[`DATA_WIDTH - 1])
+                                    || ( A[`DATA_WIDTH - 1] &&  B[`DATA_WIDTH - 1] && ~sum[`DATA_WIDTH - 1] && B_Tmin)
                     : `DATA_WIDTH'bx;
 
-    assign Overflow = (ALUop == ADD || ALUop == SUB) ? (A2[`DATA_WIDTH - 1] && B2[`DATA_WIDTH - 1] && ~sum[`DATA_WIDTH - 1]) || (~A2[`DATA_WIDTH - 1] && ~B2[`DATA_WIDTH - 1] && sum[`DATA_WIDTH - 1])
+    assign Overflow = (ALUop == ADD) ? ( A[`DATA_WIDTH - 1] &&  B[`DATA_WIDTH - 1] && ~sum[`DATA_WIDTH - 1])
+                                    || (~A[`DATA_WIDTH - 1] && ~B[`DATA_WIDTH - 1] &&  sum[`DATA_WIDTH - 1])
+                    : (ALUop == SUB) ? (~A[`DATA_WIDTH - 1] &&  B[`DATA_WIDTH - 1] &&  sum[`DATA_WIDTH - 1])
+                                    || ( A[`DATA_WIDTH - 1] && ~B[`DATA_WIDTH - 1] && ~sum[`DATA_WIDTH - 1])
                     : `DATA_WIDTH'bx;
 
     assign     Zero = (Result == `DATA_WIDTH'b0);
