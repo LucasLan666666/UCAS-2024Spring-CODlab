efdf82c44455f90a45293da7dfd957ab8cccf21a
Lucas <lanyuzhou22@mails.ucas.ac.cn>
2024-03-05 19:58:52 +0800

    alu modified

--- fpga/design/ucas-cod/hardware/sources/alu/alu.v
+++ fpga/design/ucas-cod/hardware/sources/alu/alu.v
@@ -18,38 +18,42 @@ module alu (
     localparam SUB = 3'b110;
     localparam SLT = 3'b111;
 
-    wire   [`DATA_WIDTH - 1:0]  B2;
+    wire   [`DATA_WIDTH - 1:0]  B_trans;
     wire                        cout;
-    wire   [`DATA_WIDTH - 1:0]  sum;
+    wire   [`DATA_WIDTH - 1:0]  S;
+    wire                        sign_A;
+    wire                        sign_B;
+    wire                        sign_;
 
     adder_32 alu_adder (
         .A(          A),
-        .B(         B2),
+        .B(    B_trans),
         .cin( b_invert),
         .cout(    cout),
-        .sum(      sum)
+        .sum(        S)
     );
 
     // for subtraction
-    assign       B2 = (ALUop == SUB || ALUop == SLT) ? ~B : B;
-    assign b_invert = (ALUop == SUB || ALUop == SLT) ? 1'b1 : 1'b0;
+    assign  B_trans =  (ALUop == SUB) || (ALUop == SLT) ? ~B : B;
+    assign b_invert =  (ALUop == SUB) || (ALUop == SLT);
+    // for Status Flags
+    assign   sign_A =  A[`DATA_WIDTH - 1];
+    assign   sign_B =  B[`DATA_WIDTH - 1];
+    assign   sign_S =  S[`DATA_WIDTH - 1];
 
-    assign Overflow = (ALUop == ADD) ? ((A[`DATA_WIDTH - 1] == B[`DATA_WIDTH - 1]) && (A[`DATA_WIDTH - 1] ^ sum[`DATA_WIDTH - 1]))
-                    : (ALUop == SUB || ALUop == SLT) ? ((A[`DATA_WIDTH - 1] ^ B[`DATA_WIDTH - 1]) && (A[`DATA_WIDTH - 1] ^ sum[`DATA_WIDTH - 1]))
-                    : `DATA_WIDTH'bx;
+    assign Overflow =  (ALUop == ADD) && (sign_A == sign_B) && (sign_A ^ sign_S)
+                    || (ALUop == SUB || ALUop == SLT) && (sign_A ^ sign_B) && (sign_A ^ sign_S);
 
-    assign CarryOut = (ALUop == ADD) ? cout
-                    : (ALUop == SUB) ? (~A[`DATA_WIDTH - 1] && B[`DATA_WIDTH - 1])
-                                    || ((A[`DATA_WIDTH - 1] == B[`DATA_WIDTH - 1]) && sum[`DATA_WIDTH - 1])
-                    : `DATA_WIDTH'bx;
+    assign CarryOut =  (ALUop == ADD) && cout
+                    || (ALUop == SUB) && ((~sign_A && sign_B) || (sign_A == sign_B) && sign_S);
 
-    assign     Zero = (Result == `DATA_WIDTH'b0);
+    assign     Zero =  (Result == `DATA_WIDTH'b0);
 
-    assign   Result = (ALUop == AND) ? A & B
-                    : (ALUop == OR ) ? A | B
-                    : (ALUop == ADD || ALUop == SUB) ? sum
-                    : (ALUop == SLT) ? sum[`DATA_WIDTH - 1] ^ Overflow
-                    : `DATA_WIDTH'bx;
+    assign   Result =  (ALUop == AND) ? A & B
+                    :  (ALUop == OR ) ? A | B
+                    :  (ALUop == ADD || ALUop == SUB) ? S
+                    :  (ALUop == SLT) ? sign_S ^ Overflow
+                    :  `DATA_WIDTH'b0;
 endmodule
 
 module adder_32 (
