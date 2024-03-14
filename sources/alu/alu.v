ba85a3c526aefd480a82c52cd6eecb5b3950842b
Yuzhou Lan <lanyuzhou22@mails.ucas.ac.cn>
2024-03-14 19:25:02 +0800

    simplified

---
+++ sources/alu/alu.v
@@ -0,0 +1,66 @@
+`timescale 10 ns / 1 ns
+
+`define DATA_WIDTH 32
+
+module alu (
+    input  [`DATA_WIDTH - 1:0]  A,
+    input  [`DATA_WIDTH - 1:0]  B,
+    input  [              2:0]  ALUop,
+    output                      Overflow,
+    output                      CarryOut,
+    output                      Zero,
+    output [`DATA_WIDTH - 1:0]  Result
+);
+
+    // Status Flags
+    localparam AND = 3'b000;
+    localparam  OR = 3'b001;
+    localparam ADD = 3'b010;
+    localparam SUB = 3'b110;
+    localparam SLT = 3'b111;
+
+    wire   [`DATA_WIDTH - 1:0]  B_trans;
+    wire                        cout;
+    wire   [`DATA_WIDTH - 1:0]  S;
+    wire                        sign_A, sign_B, sign_S;
+
+    // the only adder, for ADD, SUB, SLT
+    adder_for_ALU alu_adder (
+        .A(          A),
+        .B(    B_trans),
+        .cin( b_invert),
+        .cout(    cout),
+        .sum(        S)
+    );
+
+    // for subtraction
+    assign  B_trans = {32{b_invert}} ^ B;
+    assign b_invert = ALUop == SUB || ALUop == SLT;
+    // for Status Flags
+    assign   sign_A = A[`DATA_WIDTH - 1];
+    assign   sign_B = B[`DATA_WIDTH - 1];
+    assign   sign_S = S[`DATA_WIDTH - 1];
+
+    assign Overflow =  (ALUop == ADD) && !(sign_A != sign_B) && (sign_A != sign_S)
+                    || b_invert && (sign_A != sign_B) && (sign_A != sign_S);
+
+    assign CarryOut =  (ALUop == ADD) && cout
+                    || (ALUop == SUB) && ((!sign_A && sign_B) || !(sign_A != sign_B) && sign_S);
+
+    assign     Zero = !Result;
+
+    assign   Result = {32{ALUop == AND}} & (A & B)
+                    | {32{ALUop == OR }} & (A | B)
+                    | {32{ALUop == ADD || ALUop == SUB}} & S
+                    | {32{ALUop == SLT}} & (sign_S != Overflow);
+endmodule
+
+module adder_for_ALU (
+    input  [`DATA_WIDTH - 1:0]  A,
+    input  [`DATA_WIDTH - 1:0]  B,
+    input                       cin,
+    output                      cout,
+    output [`DATA_WIDTH - 1:0]  sum
+);
+    assign {cout, sum} = A + B + cin;
+endmodule
---
+++ sources/reg_file/reg_file.v
@@ -0,0 +1,30 @@
+`timescale 10 ns / 1 ns
+
+`define DATA_WIDTH 32
+`define ADDR_WIDTH 5
+`define   REG_NUM 32 // the number of registers
+
+module reg_file (
+    input                       clk,
+    input  [`ADDR_WIDTH - 1:0]  waddr,
+    input  [`ADDR_WIDTH - 1:0]  raddr1,
+    input  [`ADDR_WIDTH - 1:0]  raddr2,
+    input                       wen,
+    input  [`DATA_WIDTH - 1:0]  wdata,
+    output [`DATA_WIDTH - 1:0]  rdata1,
+    output [`DATA_WIDTH - 1:0]  rdata2
+);
+
+	// reg file
+	reg [`DATA_WIDTH - 1:0] my_reg_file  [`REG_NUM - 1:0];
+
+    always @(posedge clk) begin
+        // when to accept input
+        if (wen && (waddr != `ADDR_WIDTH'b0)) begin
+            my_reg_file[waddr] <= wdata;
+        end
+    end
+    // read data from specific address
+    assign rdata1 = {`DATA_WIDTH{raddr1 != `ADDR_WIDTH'b0}} & my_reg_file[raddr1];
+    assign rdata2 = {`DATA_WIDTH{raddr2 != `ADDR_WIDTH'b0}} & my_reg_file[raddr2];
+endmodule
